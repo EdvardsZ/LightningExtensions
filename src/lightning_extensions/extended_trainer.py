429cba2ca7addd8810768138227b354d1564198c
Edvards Zakovskis <edvards1999@gmail.com>
2023-11-23 23:49:44 +0100

    added crossval

--- src/lightning_extensions/extended_trainer.py
+++ src/lightning_extensions/extended_trainer.py
@@ -1,5 +1,6 @@
 from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.loggers import TensorBoardLogger, WandbLogger
+from data_module import construct_kfold_datamodule
 import lightning as L
 import wandb
 
@@ -7,6 +8,7 @@ import wandb
 class ExtendedTrainer(L.Trainer):
     def __init__(self, project_name: str, model_name: str, max_epochs: int, devices = [5], monitor = "val_loss", **kwargs ):
         self.model_name  = model_name
+        self.project_name = project_name
 
         self._epochs = max_epochs
 
@@ -22,12 +24,52 @@ class ExtendedTrainer(L.Trainer):
         )
         super().__init__(accelerator='gpu', devices=devices, max_epochs = max_epochs, enable_progress_bar=True, callbacks=[checkpoint_callback], logger=[logger, self.wandb], **kwargs)
 
-    def fit(self, model, train_dataloader, val_dataloader):
-        super().fit(model, train_dataloader, val_dataloader)
+    def fit(self, model, train_dataloader, val_dataloader, **kwargs):
+        super().fit(model, train_dataloader, val_dataloader, **kwargs)
+        self.finish_logging()
 
     def save_model_checkpoint(self):
-        self.wandb.finalize("success")
-        wandb.finish()
         super().save_checkpoint('checkpoints/' + self.model_name + '.ckpt')
+
+    def finish_logging(self):
+        self.wandb.finalize("success")
+        wandb.finish(quiet=True)
+
+    def cross_validate(self, model, train_dataloader, val_dataloader, k = 5):
+        print("Starting crossvalidation")
+
+        data_module = construct_kfold_datamodule(train_dataloader, val_dataloader, k) 
+        ## TODO: INITIALLY NOT SHUFFLED
+
+        # checkpoint to restore from
+        # this is a bit hacky because the model needs to be saved before the fit method
+        self.strategy._lightning_module = model
+        path = "checkpoints/k_initial_weights.ckpt"
+        self.save_checkpoint(path)
+        self.strategy._lightning_module = None
+
+        results = []
+
+        for fold in range(k):
+            print("Starting fold: " + str(fold))
+            data_module.fold_index = fold
+
+            self.logger = WandbLogger(project = self.project_name, name=self.get_fold_model_name(fold), log_model="all", group = self.model_name)
+            
+            super().fit(model, data_module, ckpt_path=path)
+            self.save_model_checkpoint()
+
+            res = self.test(model=model, datamodule=data_module)
+            results.append(res)
+
+            self.finish_logging()
+            
+
+    def get_fold_model_name(self, fold):
+        return self.model_name + "_fold_" + str(fold)
+
+
+
+
         
     
